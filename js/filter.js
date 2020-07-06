e5be548353d7665768617e4c922ed9dcaa1968ab
Andrew <andersen7298@gmail.com>
2020-07-06 15:38:52 +0300

    Сделаны плавная прокрутка и рефакторинг

--- js/filter.js
+++ js/filter.js
@@ -8,21 +8,18 @@
   var typeFilterNode = document.querySelector('#housing-type');
 
   var filterPinsOfType = function (dataElement) { // функция сравнения значения фильтра с типом жилья в объявлении
-    switch (typeFilterNode.value) {
-      case (dataElement.offer.type):
-        return true;
-      case (valueOfAllAds):
-        return true;
-      default:
-        return false;
+    if (typeFilterNode.value === dataElement.offer.type || typeFilterNode.value === valueOfAllAds) {
+      return true;
     }
+
+    return false;
   };
 
   window.filter = {
     typeFilterNode: typeFilterNode,
     filterMapPins: function (data) {
-      return data.slice(0, MAX_PINS_ON_MAP). // Делаем копию массива для фильтрации
-      filter(filterPinsOfType); // Фильтрация по типу жилья
+      return data.slice(0, MAX_PINS_ON_MAP).
+      filter(filterPinsOfType);
     }
   };
 
