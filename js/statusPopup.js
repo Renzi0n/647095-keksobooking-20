ca56bfa2e08588ff56633eb63ff0328323ab8f2f
Andrew <andersen7298@gmail.com>
2020-07-10 15:13:17 +0300

    Поправки

--- js/statusPopup.js
+++ js/statusPopup.js
@@ -10,8 +10,8 @@
   var closePopup = function () {
     popupElement.remove();
 
-    document.removeEventListener('click', closePopup);
     document.removeEventListener('keydown', onPopupEscPress);
+    document.removeEventListener('click', onRandomAreaClick);
   };
 
   var onPopupEscPress = function (evt) {
@@ -20,11 +20,18 @@
     }
   };
 
+  var onRandomAreaClick = function (evt) {
+    if (evt.target !== popupElement.querySelector('p')) {
+      closePopup();
+    }
+  };
+
+
   window.renderPopup = function (isError) {
     popupElement = isError ? errorPopupNode.cloneNode(true) : successPopupNode.cloneNode(true);
 
     document.addEventListener('keydown', onPopupEscPress);
-    document.addEventListener('click', closePopup);
+    document.addEventListener('click', onRandomAreaClick);
 
     document.querySelector('main').appendChild(popupElement);
   };
